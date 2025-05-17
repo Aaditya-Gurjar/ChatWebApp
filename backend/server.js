3005d75cb38d4f5a10bf54fbe6f4b136b8bc896e
Aaditya-Gurjar <aaditya2002.com@gmail.com>
2025-05-17 09:26:03 +0530

    fixed some issues for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 const corsOptions = {
-	origin: process.env.FRONTEND_URL,
+	origin: true, // Allow all origins
 	methods: ["GET", "POST", "DELETE"],
 	allowedHeaders: ["Content-Type", "Authorization"],
 	credentials: true,
