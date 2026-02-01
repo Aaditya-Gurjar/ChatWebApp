a97e7897dd625f83fa7009eb7d82130c85e483b0
Aaditya-Gurjar <aaditya2002.com@gmail.com>
2026-02-02 00:53:32 +0530

    Fixed chat issue

--- frontend/src/utils/getChatName.js
+++ frontend/src/utils/getChatName.js
@@ -1,20 +1,33 @@
 import GroupLogo from "../assets/group.png";
+
 const getChatName = (chat, authUserId) => {
+	// Handle missing users array
+	if (!chat?.users || chat.users.length < 2) {
+		return chat?.chatName || "Unknown Chat";
+	}
+
 	const chatName =
 		chat?.chatName == "Messenger"
-			? authUserId == chat.users[0]._id
-				? chat.users[1].firstName + " " + chat.users[1].lastName
-				: chat.users[0].firstName + " " + chat.users[0].lastName
+			? authUserId == chat.users[0]?._id
+				? (chat.users[1]?.firstName || "") + " " + (chat.users[1]?.lastName || "")
+				: (chat.users[0]?.firstName || "") + " " + (chat.users[0]?.lastName || "")
 			: chat?.chatName;
-	return chatName;
+	return chatName || "Unknown";
 };
+
 export const getChatImage = (chat, authUserId) => {
+	// Handle missing users array
+	if (!chat?.users || chat.users.length < 2) {
+		return GroupLogo;
+	}
+
 	const ImageLogo =
 		chat?.chatName == "Messenger"
-			? authUserId == chat.users[0]._id
-				? chat.users[1].image
-				: chat.users[0].image
+			? authUserId == chat.users[0]?._id
+				? chat.users[1]?.image || GroupLogo
+				: chat.users[0]?.image || GroupLogo
 			: GroupLogo;
 	return ImageLogo;
 };
+
 export default getChatName;
